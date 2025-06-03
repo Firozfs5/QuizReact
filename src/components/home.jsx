106b078dfedb80401fe631196b10524e01a24a05
Firoz <firozshabir657@gmail.com>
2025-06-03 21:59:16 +0530

    mostly done

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Home=()=>{
     return (
-        <div className="bg-[#123458] w-[300px] h-64 rounded-2xl text-white flex flex-col justify-center items-center">
+        <div className="bg-[#000000] w-[300px] h-64 rounded-2xl text-white flex flex-col justify-center items-center">
             <h1 className="text-3xl mb-[10px] font-bold">Hi!</h1>
             <h1 className="text-2xl mb-[30px] font-bold">Welcome to Quizer</h1>
             <button className="text-2xl font-medium bg-green-500 p-3 rounded-xl"><Link to="/type">Let's Start</Link></button>
